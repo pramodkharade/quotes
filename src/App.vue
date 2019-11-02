f58026f1b654d943f73ddf55174501ef18666444
pramodkharade <kharade.pramod91@gmail.com>
2019-11-02 17:41:08 +0530

    added the info box for delete

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
     <div class="container">
       <app-new-quote @quoteAdded='newQuote'></app-new-quote>
       <app-quoute-grid :quotes='quotes'></app-quoute-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+            <div class="alert alert-info">
+              Info: Click on a Quote to delete it.
+            </div>
+        </div>
+      </div>
     </div>
 </template>
 
